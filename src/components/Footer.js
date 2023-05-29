52863e2e1e2b56c7a4e91ed3fa815839745fcce0
[Ezekiel Narvasa ] <[narvasa.ezekiel.bscs2020@gmail.com]>
2023-05-29 18:50:04 +0800

    .

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p> Created by Ezekiel Narvasa </p>
+    </footer>
+  );
+}
+
+export default Footer;
